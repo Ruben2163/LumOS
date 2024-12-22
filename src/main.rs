91d64b1a90c55b97068de451a5c77678cbc7c5f1
Ruben <161133456+Ruben2163@users.noreply.github.com>
2024-12-22 14:43:12 +0700

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,1 +1,24 @@
-test
+#![no_std]
+#![no_main]
+
+use core::panic::PanicInfo;
+
+#[panic_handler]
+fn panic(_info: &PanicInfo) -> ! {
+    loop {}
+}
+
+#[no_mangle]
+pub extern "C" fn _start() -> ! {
+    // Print "Hello, World!" to the screen (VGA text mode)
+    let vga_buffer = 0xb8000 as *mut u8;
+    let hello = "Hello, World!";
+    for (i, byte) in hello.bytes().enumerate() {
+        unsafe {
+            *vga_buffer.offset(i as isize * 2) = byte;
+            *vga_buffer.offset(i as isize * 2 + 1) = 0x07; // Light grey text
+        }
+    }
+
+    loop {}
+}
