b54d9f826a21ff2eb6730e15eabc073c08420a6f
Ruben <161133456+Ruben2163@users.noreply.github.com>
2024-12-22 14:45:11 +0700

    Update vga_buffer.rs

--- src/vga_buffer.rs
+++ src/vga_buffer.rs
@@ -1,1 +1,8 @@
-test
+// VGA Driver
+const VGA_BUFFER: *mut u8 = 0xB8000 as *mut u8;
+
+pub fn write_char(c: char) {
+    unsafe {
+        *VGA_BUFFER.offset(0) = c as u8; // Writing character to VGA
+    }
+}
